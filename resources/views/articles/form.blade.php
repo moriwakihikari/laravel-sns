1c7740646e208c24708120a66532424363ad1de1
moriwakihikari <daiichidaimandaikichi.gi@gmail.com>
2021-04-09 18:56:15 +0900

    GOOGLEログイン機能追加

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -6,8 +6,10 @@
 <div class="form-group">
   <article-tags-input
     :initial-tags='@json($tagNames ?? [])'
-    :autocomplete-items='@json($allTagNames ?? [])'>
+    :autocomplete-items='@json($allTagNames ?? [])'
+  >
   </article-tags-input>
+</div>
 <div class="form-group">
   <label></label>
   <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
